e58c306abc66b2e0090de3562300a6d782a1d861
vlatson647-hash <vlatson647@phm.k12.in.us>
2026-02-25 09:44:05 -0500

    Update arcade_game.py

--- arcade_game.py
+++ arcade_game.py
@@ -1,65 +1,108 @@
 import tkinter as tk
+import random
 
 WIDTH = 600
 HEIGHT = 450
-SCORE_LEFT = 0
-SCORE_RIGHT = 0
+SCORE = 0
+
+# enemy creation
+def enemy_sprite():
+    pattern = [
+        "00111111100",
+        "00111111100",
+        "00111111100",
+        "01101111010",
+        "11111111111",
+        "10111111101",
+        "10100000101",
+        "00011011000",
+    ]
+    h = len(pattern)
+    w = len(pattern[0])
+
+    img = tk.PhotoImage(width=w, height=h)
+
+    for y in range(h):
+        for x in range(w):
+            if pattern[y][x] == '1':
+                img.put('purple', (x,y))
+
+
+
+
 
 # Build window
 root = tk.Tk()
-root.title("Block Shooterz")
+root.title("BLOCK SHOOTERZ")
 
-label_left = tk.Label(root, text= 'PLAYER 1 SCORE: 0')
-label_left.pack()
-label_right = tk.Label(root, text= 'PLAYER 2 SCRORE: 0')
-label_right.pack()
+label = tk.Label(root, text= 'SCORE: 0')
+label.pack()
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='black')
 canvas.pack()
 
+enemy_img = enemy_sprite()
+
 # player creation
-player1 = canvas.create_rectangle(50, 160, 60, 250, fill= 'red')
-player2 = canvas.create_rectangle(540, 150, 550, 250, fill= 'blue')
+player = canvas.create_rectangle(60, 100, 70, 200, fill= 'white')
 
 # enemy list
 enemies = []
 
-
-# status booleans
-alive1 = True
-alive2 = True
+alive = True
 
 
 # movement
-def one_up(event):
-    canvas.move(player1, 0,  -25)
+def move_up(event):
+    canvas.move(player, 0,  -25)
 
-def one_down(event):
-    canvas.move(player1, 0, 25)
+def move_down(event):
+    canvas.move(player, 0, 25)
 
-def two_up(event):
-    canvas.move(player2, 0, -25)
-
-def two_down(event):
-    canvas.move(player2, 0, 25)
 
 
 # binds
-canvas.bind_all('<w>', one_up)
-canvas.bind_all('<s>', one_down)
-canvas.bind_all('<Up>', two_up)
-canvas.bind_all('<Down>', two_down)
+root.bind('<Up>', move_up)
+root.bind('<Down>', move_down)
 
 
 
+# spawns enemies
+def spawn_enemy():
+    x = random.randint(0, WIDTH-20)
+    enemy = canvas.create_image(x,50, image=enemy_img, anchor = 'nw')
+    enemies.append(enemy)
 
 
+# runs game
+def run_game():
+    global SCORE
+    global alive
+
+    if not alive:
+        canvas.delete('all')
+        root.unbind('<Up>')
+        root.unbind('<Down>')
+    if not alive:
+        canvas.create_text(WIDTH//2, HEIGHT//2, text="DO NOT MOVE OUT!", fill='red', font=('arial',24))
+        return
+    
+    px1, py1, px2, py2 = canvas.bbox(player)
+
+    if py1 < 1 or py2 > 451:
+        alive = False
+    if random.randint(1,5)==1:
+        spawn_enemy()
+
+    for enemy in enemies:
+        canvas.move(enemy, 0, 10)
 
 
-
+    root.after(30, run_game)
 
 
 
 
 
 # runs game/shows window
+run_game()
 root.mainloop()
