c01fc008a292b89ea81f7caaed7e5a27a324277b
vlatson647-hash <vlatson647@phm.k12.in.us>
2026-02-27 09:44:04 -0500

    Update arcade_game.py

--- arcade_game.py
+++ arcade_game.py
@@ -4,6 +4,10 @@ import random
 WIDTH = 600
 HEIGHT = 450
 SCORE = 0
+timer = 60
+frame_rate = .04
+count = 60
+
 
 # enemy creation
 def enemy_sprite():
@@ -11,7 +15,7 @@ def enemy_sprite():
         "00111111100",
         "00111111100",
         "00111111100",
-        "01101111010",
+        "01201110210",
         "11111111111",
         "10111111101",
         "10100000101",
@@ -25,9 +29,18 @@ def enemy_sprite():
     for y in range(h):
         for x in range(w):
             if pattern[y][x] == '1':
-                img.put('purple', (x,y))
+                img.put('red', (x,y))
+            if pattern[y][x] == '2':
+                img.put('lime green', (x,y))
+    return img
 
-
+def countdown(count):
+    label.config(text=count)
+    
+    if count > 0:
+        root.after(1000, countdown, count - 1)
+    else:
+        label.config(text="Time's up!")
 
 
 
@@ -35,15 +48,17 @@ def enemy_sprite():
 root = tk.Tk()
 root.title("BLOCK SHOOTERZ")
 
-label = tk.Label(root, text= 'SCORE: 0')
-label.pack()
+time = tk.Label(root, text='', font=('Arial', 10))
+time.pack(side='top', fill='x')
+label = tk.Label(root, text='', font=('Arial', 10))
+label.pack(side='bottom', fill='x')
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='black')
 canvas.pack()
 
 enemy_img = enemy_sprite()
 
 # player creation
-player = canvas.create_rectangle(60, 100, 70, 200, fill= 'white')
+player = canvas.create_rectangle(30, 50, 35, 100, fill= 'white')
 
 # enemy list
 enemies = []
@@ -68,15 +83,17 @@ root.bind('<Down>', move_down)
 
 # spawns enemies
 def spawn_enemy():
-    x = random.randint(0, WIDTH-20)
-    enemy = canvas.create_image(x,50, image=enemy_img, anchor = 'nw')
+    x = random.randint(WIDTH-200, WIDTH-20)
+    enemy = canvas.create_image(x,0, image=enemy_img)
     enemies.append(enemy)
+
 
 
 # runs game
 def run_game():
     global SCORE
     global alive
+
 
     if not alive:
         canvas.delete('all')
@@ -90,11 +107,12 @@ def run_game():
 
     if py1 < 1 or py2 > 451:
         alive = False
-    if random.randint(1,5)==1:
+    if random.randint(1,1)==1:
         spawn_enemy()
 
     for enemy in enemies:
-        canvas.move(enemy, 0, 10)
+        canvas.move(enemy, 0, 8)
+    
 
 
     root.after(30, run_game)
@@ -103,6 +121,8 @@ def run_game():
 
 
 
+
 # runs game/shows window
 run_game()
+root.after(0, countdown, count)
 root.mainloop()
